a8c9a8e2817d817a792d503668127361e0233296
Saiid El Hajj Chehade <saidhajjchehade@gmail.com>
2020-05-19 17:13:15 +0300

    Add Saiid Art files

---
+++ GameProjectMay2020/Assets/Chapters/Chapter 5 - Uncle Beek/Scripts/CoilDamageable.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoilDamageable : MonoBehaviour
+{
+
+    Animator animator;
+    GameObject parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Connect script to empty child to act as trigger and reference parent
+        parent = gameObject.transform.parent.gameObject;
+        animator = parent.GetComponent<Animator>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+        if(other.tag == "Player")
+        {
+            //Animation Stuff
+            animator.SetBool("activated",true);
+
+            //CODE FOR DAMAGING THE PLAYER...
+        }
+
+
+    }
+    void OnTriggerExit2D()
+    {
+
+        animator.SetBool("activated", false);
+
+    }
+
+    
+}
---
+++ GameProjectMay2020/Assets/Chapters/Chapter 5 - Uncle Beek/Scripts/movingPlatformController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class movingPlatformController : MonoBehaviour
+{
+    //public parameters
+
+    public float horizontal_amplitude = 1f;
+    public float horizontal_period = 1f;
+    public float vertical_amplitude = 1f;
+    public float vertical_period = 1f;
+    public float vertical_phase = 1f;
+
+    Vector2 initialPos;
+    float t = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+
+        Vector2 pos = transform.position;
+
+
+        //Oscillate horizontally
+        pos.x = initialPos.x + (float)(horizontal_amplitude * Math.Sin(t / horizontal_period));
+
+        //Oscillate vertically
+        pos.y = initialPos.y + (float)(vertical_amplitude * Math.Sin(t / vertical_period+vertical_phase));
+
+        //submit position
+        transform.position = pos;
+
+    }
+
+    
+}
